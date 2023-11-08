ef6d31f59e7e7850f73fe3efa29bcf035b097b60
Maide <maideergul@yahoo.com>
2023-11-08 17:06:54 +0300

    v1

---
+++ currency-exchange-service/src/main/java/com/works/springcurrencyexchangeservice/CurrencyExchangeController.java
@@ -0,0 +1,40 @@
+package com.works.springcurrencyexchangeservice;
+
+import java.math.BigDecimal;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CurrencyExchangeController {
+
+	private Logger logger = LoggerFactory.getLogger(CurrencyExchangeController.class);
+	
+	@Autowired
+	private CurrencyExchangeRepository repo;
+	
+	@Autowired
+	private Environment environment;
+	
+	@GetMapping("/currency-exchange/from/{from}/to/{to}")
+	public CurrencyExchange retrieveExchangeValue(@PathVariable String from, @PathVariable String to)
+	{
+		//CurrencyExchange currencyExchange = new CurrencyExchange(1000L, from, to, BigDecimal.valueOf(50));
+		
+		logger.info("retrieveExchangeValue called with {} to {}", from, to);
+		
+		CurrencyExchange currencyExchange = repo.findByFromAndTo(from, to);	
+		if(currencyExchange == null)
+		{
+			throw new RuntimeException("Unable to find data for : " +from + " to " +to);
+		}
+		currencyExchange.setEnvironment(environment.getProperty("local.server.port"));
+		
+		return currencyExchange;
+	}
+}
---
+++ currency-exchange-service/src/main/java/com/works/springcurrencyexchangeservice/CurrencyExchangeRepository.java
@@ -0,0 +1,9 @@
+package com.works.springcurrencyexchangeservice;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CurrencyExchangeRepository extends JpaRepository<CurrencyExchange, Long> {
+
+	CurrencyExchange findByFromAndTo(String from, String to);
+	
+}
